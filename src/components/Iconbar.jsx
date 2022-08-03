3521c90410052f58c6582cbf76e9f1982eeb5ca7
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-08-03 17:49:04 +0200

    done for today aug3

--- src/components/Iconbar.jsx
+++ src/components/Iconbar.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
 import {
-  AktiBusfahren,
-  AktiChattenn,
-  AktiFahrradfahren,
-  AktiLesen,
-  AtmoAngespannt,
-  AtmoKonzentriert,
-  AtmoLustig,
-  AtmosSchoen,
-  OrteArzt,
-  OrteBeiFreundIn,
-  OrteSchule,
-  OrteZuHause,
-  PersonenEltern,
-  PersonenFreunde,
-  PersonenLehrerin,
-  PersonenSchwester,
-} from "../assets/svgr_output/index";
+  personen,
+  aktis,
+  orte,
+  atmos,
+} from "../assets/svgr_output/ObjectIcons";
+
+const objectIconArray = [personen, aktis, orte, atmos];
 
 export default function Iconbar() {
   const svgConvaEvent = (i) => {
@@ -26,43 +16,28 @@ export default function Iconbar() {
 
   return (
     <div className="iconBarContainer">
-      <ScrollContainer className="scroll-container iconToolbarRow spaces" id="xDragToolbar" >
-        <OrteArzt />
-        <OrteBeiFreundIn />
-        <OrteSchule />
-        <OrteZuHause />
-      </ScrollContainer>
-      {/* <ScrollContainer
-        className="scroll-container iconToolbarRow transport"
-        id="xDragToolbar"
-      ></ScrollContainer>  */}
-      <ScrollContainer
-        className="scroll-container iconToolbarRow people"
-        id="xDragToolbar" horizontal="true" vertical="false"
-      >
-      <PersonenEltern />
-      <PersonenFreunde />
-      <PersonenLehrerin />
-      <PersonenSchwester />
-      </ScrollContainer>
-      <ScrollContainer
-        className="scroll-container iconToolbarRow actvities"
-        id="xDragToolbar" horizontal="true" vertical="false"
-      >
-      <AktiBusfahren />
-      <AktiChattenn />
-      <AktiFahrradfahren />
-      <AktiLesen />
-      </ScrollContainer>
-      <ScrollContainer
-        className="scroll-container iconToolbarRow atmosphere"
-        id="xDragToolbar" horizontal="true" vertical="false"
-      >
-        <AtmoAngespannt />
-        <AtmoKonzentriert />
-        <AtmoLustig />
-        <AtmosSchoen />
-  </ScrollContainer>
+      {objectIconArray.map((object, i) => {
+        return (
+          <ScrollContainer
+            key={i}
+            className={`scroll-container iconToolbarRow ${object}`}
+            id="xDragToolbar"
+          >
+            {Object.keys(object).map(function (icon, j) {
+              return React.createElement(
+                object[icon],
+                {
+                  key: j,
+                  onClick: () => {
+                    svgConvaEvent(icon);
+                  },
+                },
+                null
+              );
+            })}
+          </ScrollContainer>
+        );
+      })}
     </div>
   );
 }

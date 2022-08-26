3f160076302afd4b697820900e665a813a09c437
zloc-alz <alza@zloc.ws>
2022-08-26 19:27:25 +0200

    dragPosition saving not working yet

--- src/components/KonvaCanvas.jsx
+++ src/components/KonvaCanvas.jsx
@@ -7,26 +7,47 @@ const KonvaCanvas = () => {
   const stageRef = useRef();
   const percentWidth = (window.innerWidth / 100) * 65;
   const [images, setImages] = useState([
-    { id: 0, icon: "", x: percentWidth / 2, y: window.innerHeight / 2 },
+    { id: 0, icon: "" },
   ]);
-  console.log(images);
 
+  const imgPos = {
+    x: 300,
+    y: 300,
+  }
+
+  const handleDragStart = (e) => {
+    e.target.setAttrs({
+      scaleX: 1.1,
+      scaleY: 1.1,
+    });
+  }
+
+  const handleDragEnd = (e) => {
+    e.target.to({
+      duration: 0.2,
+      easing: Konva.Easings.EaseInOut,
+      scaleX: 1,
+      scaleY: 1,
+    });
+    console.log(e.target.attrs)
+    // setImages((current) => [...current, { id: current.length, icon: e.target.attrs.image.src }]);
+  }
+
+  console.log(`Logging images var: ${images}`);
+
+
+  const [] = useState([])
   const addImages = (obj) => {
     setImages((current) => [...current, obj]);
+    // setImages((current) => [...current, {
+    //   x: e.target.x(),
+    //   y: e.target.y(),
+    // }
+    // ]);
+    let stage = stageRef.current;
   };
 
-  let posHist = [
-    {
-      x: 0,
-      y: 0,
-    },
-  ];
-  const [pos, setPos] = useState({
-    position: posHist[0],
-  });
-
   const URLImage = ({ image }) => {
-
 
     const [img] = useImage(image);
     const [canvasPos, setCanvasPos] = useState({
@@ -40,31 +61,37 @@ const KonvaCanvas = () => {
         // ref={imgRef}
         image={img}
         draggable="true"
-        
+        x={imgPos.x}
+        y={imgPos.y}
         // I will use offset to set origin to the center of the image
         offsetX={img ? img.width / 2 : 0}
         offsetY={img ? img.height / 2 : 0}
         // onDragMove={(e) => console.log(e.target.attrs.x)}
-        onDragStart={(e) => {
-          // setImages(
-          //   (img.x = e.target.attrs.x),
-          //   (img.y = e.target.attrs.y)
-          // );
-        }}
-        onDragEnd={(e) => {
-          console.log(img);
-          // setImages(
-          //   (img.x = e.target.attrs.x),
-          //   (img.y = e.target.attrs.y)
-          // );
-        }}
+        // draggable
+        shadowBlur={5}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        // onDragEnd={
+        //   (e) => {
+        //     setImages((current) => [...current, {
+
+
+        //       // x: e.target.x(),
+        //       // y: e.target.y(),
+        //     }
+        //     ]);
+        //   }}
+        // console.log(e.target.attrs.x);
+        // console.log(e.target.attrs.y);
+        scaleX={Image.isDragging ? 1.5 : 1}
       />
     );
   };
 
   return (
     <>
-      <div className="konvaContainer">
+      <div className="konvaContainer"
+      >
         <Stage width={percentWidth} height={window.innerHeight} ref={stageRef}>
           <Layer>
             {images.map((img) => {
@@ -72,8 +99,6 @@ const KonvaCanvas = () => {
                 <URLImage
                   image={img.icon}
                   key={img.id}
-                  x={img.x}
-                  y={img.y}
                 />
               );
             })}

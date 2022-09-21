f796d72235402dbb2e50c85f15c1259b9eb96c59
zloc-alz <alza@zloc.ws>
2022-09-21 11:32:41 +0200

    give theme selection buttons their respective colors

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Box, Button, ButtonGroup } from "@mui/material";
+import { Box, Button, ButtonGroup, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { CustomThemeContext } from "../themes/CustomThemeProvider";
 import "../Fonts.css";
-
-
 
 const ThemeSelector = () => {
 
@@ -16,32 +14,36 @@ const ThemeSelector = () => {
             zIndex: 1,
             margin: "1rem"
         }}>
-
             <ButtonGroup
                 orientation="vertical"
                 aria-label="vertical outlined button group"
-                variant="contained"
+                variant="outlined"
             >
                 <Button
                     sx={{
                         fontFamily: "Poppins",
                         fontWeight: "bold",
+                        color: "#7dd1a4",
+                        borderColor: "#000000",
                     }}
                     onClick={() => { setTheme("regular") }}>Regular</Button>
                 <Button
                     sx={{
                         fontFamily: "Space Mono",
                         fontWeight: "bold",
+                        borderColor: "#000000",
+                        color: "#000000",
                     }}
                     onClick={() => { setTheme("softice") }}>Soft Ice</Button>
                 <Button
                     sx={{
                         fontFamily: "Ark-Es",
                         fontWeight: "bold",
+                        borderColor: "#000000",
+                        color: "#ff0000",
                     }}
                     onClick={() => { setTheme("space") }}>Space</Button>
             </ButtonGroup>
-
         </Box>
     )
 }

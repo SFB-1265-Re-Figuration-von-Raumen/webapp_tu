c36b663d16e7487f33c714b498c1f89f827a561f
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-09-27 16:54:24 +0200

    vite config base fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "https://webapp-tu-v96w.vercel.app/",
   build: {
     outDir: "./dist",
     // emptyOutDir: true,

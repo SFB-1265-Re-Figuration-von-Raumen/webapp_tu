18e5fbb3a0eaaeb07055f18cef92c4147c7a2ab4
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-09-27 18:14:42 +0200

    path fixes 37

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "webapp-tu-v96w.vercel.app/",
+  base: "/",
   build: {
     outDir: "./dist",
     // emptyOutDir: true,

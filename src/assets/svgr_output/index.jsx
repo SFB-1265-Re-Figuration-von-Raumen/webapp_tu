4fecbb420ac5cdab68818cfa3934d20d5638ee4c
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-09-29 18:11:35 +0200

    new try

---
+++ src/assets/svgr_output/index.jsx
@@ -0,0 +1,4 @@
+export { default as AktiBusfahren } from "./AktiBusfahren";
+export { default as AktiChattenn } from "./AktiChattenn";
+export { default as AktiFahrradfahren } from "./AktiFahrradfahren";
+export { default as AktiLesen } from "./AktiLesen";

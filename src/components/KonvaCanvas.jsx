7618333d24c7cbd4b03473a75ac7c0d9a6339c48
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-11-04 16:11:43 +0100

    revert missing index.js

--- src/components/KonvaCanvas.jsx
+++ src/components/KonvaCanvas.jsx
@@ -454,13 +454,7 @@ const KonvaCanvas = () => {
           </Button>
           <div className="export--btn">
             <Button variant="outlined" color="secondary" onClick={handleExport}>
-              <span style={{color: theme.palette.primary.main}}>Export</span>
-              <UIcons.Svg133DText
-                alt="Export Button"
-                className="zoomyZoom"
-                width={null}
-                height={null}
-              />
+              Export Canvas
             </Button>
           </div>
         </div>

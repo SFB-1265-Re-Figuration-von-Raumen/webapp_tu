debb2112ea33821166c2a2107cf687604464f900
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-11-08 14:44:24 +0100

    getStage()

--- src/components/KonvaCanvas.jsx
+++ src/components/KonvaCanvas.jsx
@@ -161,7 +161,7 @@ const KonvaCanvas = () => {
     console.log(touch1);
     console.log(touch2);
 
-    const stage = stageRef.getStage();
+    const stage = stageRef.current.target.getStage();
     console.log(stage);
 
     if (touch1 && touch2) {
